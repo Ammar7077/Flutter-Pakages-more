e9118412172a35486d60f35952e2213629cc39d2
Ammar Omari <64936485+Ammar7077@users.noreply.github.com>
2023-09-07 04:41:34 +0300

    Create languages_class.dart

---
+++ your_flutter_app/lib/core/localization/languages_class.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/cupertino.dart';
+
+class Languages {
+  //
+  static const Locale english = Locale('en');
+  static const Locale spanish = Locale('es');
+  static const Locale arabic = Locale('ar');
+  static const Locale turkish = Locale('tr');
+  static const Locale russian = Locale('ru');
+  //
+  static List<String> languages = [
+    'English',
+    'Spanish',
+    'عربي',
+    'Turkish',
+    'Russian',
+  ];
+  //
+  static const all = [
+    english,
+    spanish,
+    arabic,
+    turkish,
+    russian,
+  ];
+}
